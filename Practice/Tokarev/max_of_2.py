f74e29d674708ca6b398598cf6b05149151c01c9
TokarevMI <mikhail.tokarev@stm-labs.ru>
2021-06-24 21:11:31 +0300

    Задание 2 (вывод на экран + возвращение большего числа из двух)

---
+++ Practice/Tokarev/max_of_2.py
@@ -0,0 +1,10 @@
+def max_of_int(a, b):
+    if a > b:
+        print(f'Максимальное число из введенных a: {a}')
+        return a
+    else:
+        print(f'Максимальное число из введенных b: {a}')
+        return b
+
+
+print(f'Максимальное число из введенных: {max_of_int(5, 4)}')
