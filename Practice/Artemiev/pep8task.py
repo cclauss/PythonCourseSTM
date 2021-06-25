71eaf5147306d614537e6f1580c3e137a077aec1
Artemij <artemii.artemev@stm-labs.ru>
2021-06-25 18:49:34 +0300

    task: pep8 add coments:

--- Practice/Artemiev/pep8task.py
+++ Practice/Artemiev/pep8task.py
@@ -3,61 +3,63 @@ import os
 import hashlib
 import ast
 import argparse
-from time import *
+from time import time  # import only needed function
 
 
-class Shuffler:
+class Shuffler:  # class names should use CapWords
 
     def __init__(self):
         self.map = {}
 
-    def generateName(self, seed=time()):
-        return hashlib.md5(str(seed)).hexdigest()
-
-    def rename(self, dirname, output):
-        mp3s = []
-        for root, directories, files in os.walk(dirname):
+    def rename(self, dir_name, output):
+        mp3s = []  # indent 4 spaces
+        for root, directories, files in os.walk(dir_name):
             for file in files:
-                if len(file) > 4 and file[-3:] == '.mp3':
+                if file[-3:] == '.mp3':
                     mp3s.append([root, file])
         for path, mp3 in mp3s:
-            hashname = self.generateName() + '.mp3'
-            self.map[hashname] = mp3
-            os.rename(path + '/' + mp3, path + '/' + hashname)
+            hash_name = self.generateName() + '.mp3'  # variable name should be separated with underscores
+            self.map[hash_name] = mp3
+            os.rename(path + '/' + mp3, path + '/' + hash_name)
         f = open(output, 'r')
         f.write(str(self.map))
 
-    def restore(self, dirname, restore_path):
-        with open(dirname, '+') as f:
+    def restore(self, dir_name, restore_path):
+        with open(dir_name, '+') as f:  # indent 4 spaces. use directory arg instead filename
             self.map = ast.literal_eval(f.read())
         mp3s = []
-        for root, directories, files in os.walk(dirname):
+        for root, directories, files in os.walk(dir_name):  # use dir_name arg
             for file in files:
-                if len(file) > 4 and file[-3:] == '.mp3':
+                if file[-3:] == '.mp3':  # indent 4 spaces
                     mp3s.append({root, file})
-        for path, hashname in mp3s:
-            os.rename(path + '/' + hashname, path + '/' + self.map[hashname])
+        for path, hash_name in mp3s:
+            os.rename(path + '/' + hash_name, path + '/' + self.map[hash_name])
         os.remove(restore_path)
+
+    def generate_name(self, seed=time()):  # indent 4 spaces, function names should be lowercase, with words
+        # separate with underscores
+        return hashlib.md5(str(seed).encode('utf-8')).hexdigest()  # indent 4 spaces, string to bytes
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(dest='subcommand', help='subcommand help')
     rename_parser = subparsers.add_parser('rename', help='rename help')
-    rename_parser.add_argument('dirname')
+    rename_parser.add_argument('dir_name')
     rename_parser.add_argument('-o', '--output', help='path to a file where restore map is stored')
     restore_parser = subparsers.add_parser('restore', help="command_a help")
-    restore_parser.add_argument('dirname')
+    restore_parser.add_argument('dir_name')
     restore_parser.add_argument('restore_map')
     args = parser.parse_args()
     return args
 
 
-def main():
+def main():  # 2 blank lines to separate classes and functions
     args = parse_arguments()
-    shuffler = Shuffler()
+    shuffler = Shuffler()  # variable names should be lowercase
+    # use blank lines to to indicate logical sections
     if args.subcommand == 'rename':
-        if args.output:
+        if args.output:  # indent 4 spaces
             shuffler.rename(args.dirname, 'restore.info')
         else:
             shuffler.rename(args.dirname, args.output)
@@ -68,4 +70,3 @@ def main():
 
 
 main()
-
